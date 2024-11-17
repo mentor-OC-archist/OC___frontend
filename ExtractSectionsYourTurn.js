ab09ec554778fd5b8e5459713e28c32118dfb596
achicyr <hi.cyril@gmail.com>
2024-11-17 11:03:01 +0100

    LATE WEEK PUSH

---
+++ ExtractSectionsYourTurn.js
@@ -0,0 +1,2 @@
+a=Array.from(Array.from(document.querySelectorAll(".foldable")).at(-2).querySelectorAll("h4")).at(-1)
+a.parentElement.innerHTML.substring(a.parentElement.innerHTML.indexOf(a.outerHTML))
